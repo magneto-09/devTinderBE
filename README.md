17ee5e7f367dadf8bcff5e030a88eddbd53c9096
Piyush Sharma <92093386+magneto-09@users.noreply.github.com>
2025-04-05 14:31:08 +0530

    Readme change (#9)
    * Update README.md (#1)
    
    * Update README.md (#2)
    
    * Basic Things:- (#3) (#4)
    
    * Connect DB to nodeApp at first.
    * Make the nodeApp to listen at PORT No.
    * Create userModel
    * Create 2 different users and save to user collection.
    
    * Update README.md (#5)
    
    * Update README.md (#6)
    
    * Update README.md (#7)

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
 # DevTinder
 
 ## For Development - Branching & PR Workflow
-* Always start by creating a new branch from GitHub (this creates a remote branch).
-* Check out that remote branch locally.
-* Create a local child branch from the remote branch to make your changes.
-* Raise a PR from the local child branch to the remote parent branch (base).
-* Once the PR is approved, it can be merged — direct pushes are not allowed due to branch protection rules.
+* **NAMING CONVENTION**
+  * For **features**:
+    * Use the `feature-` prefix.
+    * Example: feature-login, feature-user-auth
+  * For **bug fixes**:
+    * Use the `hotfix-` prefix.
+    * Example: hotfix-crash-on-submit, hotfix-ui-glitch
+
+* `main`, `readme`, and **any branch** starting with `feature-` or `hotfix-` are protected — you **cannot commit or push directly** to these branches due to **branch protection rules**.
+
+* To **push the changes**:- 
+  * Check out that remote branch locally.
+  * Create a local child branch from the remote branch to make your changes.
+  * Raise a PR from the local child branch to the remote parent branch (base).
+  * Once the PR is approved, it can be merged.
--- src/app.js
+++ src/app.js
@@ -1,15 +1,53 @@
-const express = require('express'); 
+const express = require('express');
+const dotenv = require('dotenv');
+const { connectDB } = require('./config/db')
+require('colors'); // for colorful clg(s)
 
-const app=express();
+const app = express();
+
+dotenv.config(); // loads all environment Variables
 
 
-app.listen(7777, () => {
-    console.log("Server listens at Port 7777....")
+// routes
+const { userModel: User } = require('./models/user');
+
+app.post('/signup', async (_, res, next) => {
+    const userObj = {
+        firstName: 'Piyush',
+        lastName: 'Sharma',
+        email: 'spiyush1721@gmail.com',
+        phone: 9123118239,
+        age: 23
+    }
+
+    const newUser = new User(userObj); //instance of model --> document
+
+    try {
+        await newUser.save();
+        console.log('User added successfully'.bgGreen.white)
+        res.status(200).send(JSON.stringify(userObj));
+    } catch (error) {
+        console.log(`Error occured. ${error}`.bgRed.yellow)
+        next(error);
+    }
 })
 
-app.get("/", (req, res) => {
-    res.send("Hello World from dashboard!!!");
-})
 
+// Home route to indicate server is working perfectly fine. 
+app.get('/', (_, res) => res.send('<h1>devTinder Server is up & running.</h1>'))
 
+const PORT = process.env.PORT || 7777
+
+// Always connect your nodeApp with DB at first then make your nodeApp to listen at PORT NO. 
+connectDB().then(
+    (connData) => {
+        // using optional chaining
+        console.log(`Connected to mongoDB database ${connData?.connection?.host}`.bgMagenta.white);
+
+        app.listen(PORT, () => console.log(`Server started listening at PORT No. ${PORT}`.bgWhite.black))
+    }
+)
+    .catch(
+        (e) => console.log(`Failed to connect to DB. ${e}`.bgRed.yellow)
+    )
 
